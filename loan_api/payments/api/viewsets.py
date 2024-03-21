318ebfa1537f99b607bede0c491502cca7fa4870
Hugo Alves <hugo.asb96@gmail.com>
2024-03-21 17:59:29 -0300

    Refactor list loans viewsets

--- loan_api/payments/api/viewsets.py
+++ loan_api/payments/api/viewsets.py
@@ -6,7 +6,23 @@ from payments.api.serializers import PaymentSerializer
 from loan_api.paginations import CustomPagination
 
 
-class PaymentViewSet(ReadOnlyModelViewSet):
+class ListAllPaymentsViewSet(ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated]
+    pagination_class = CustomPagination
+    serializer_class = PaymentSerializer
+
+    def get_queryset(self):
+        loans_id = Loan.objects.filter(user=self.request.user).values_list(
+            "id", flat=True
+        )
+        return Payment.objects.filter(loan__in=loans_id).order_by("-date")
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
+
+
+class ListPaymentsByLoanViewSet(ReadOnlyModelViewSet):
     permission_classes = [IsAuthenticated]
     pagination_class = CustomPagination
     serializer_class = PaymentSerializer
@@ -14,11 +30,6 @@ class PaymentViewSet(ReadOnlyModelViewSet):
     def get_queryset(self):
         if "loan_id" in self.kwargs:
             return Payment.objects.filter(loan=self.kwargs["loan_id"]).order_by("-date")
-
-        loans_id = Loan.objects.filter(user=self.request.user).values_list(
-            "id", flat=True
-        )
-        return Payment.objects.filter(loan__in=loans_id).order_by("-date")
 
     def perform_destroy(self, instance):
         instance.is_active = False
--- loan_api/payments/urls.py
+++ loan_api/payments/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from payments.api.viewsets import PaymentViewSet
+from payments.api.viewsets import ListAllPaymentsViewSet, ListPaymentsByLoanViewSet
 from payments import views
 
 
 router = DefaultRouter()
-router.register("list_all", PaymentViewSet, basename="list_all_payments")
+router.register("list_all", ListAllPaymentsViewSet, basename="list_all_payments")
 router.register(
-    "list/(?P<loan_id>[^/.]+)", PaymentViewSet, basename="list_payments_by_loan_id"
+    "list/(?P<loan_id>[^/.]+)",
+    ListPaymentsByLoanViewSet,
+    basename="list_payments_by_loan_id",
 )
 
 urlpatterns = [
